72187d95e30c9fc4208bbe04f8d7c788449299b6
Anna Blackwell <annablackwell44@gmail.com>
2015-10-18 15:14:50 +1300

    Add first few answers

---
+++ functions.js
@@ -0,0 +1,35 @@
+1. Cube
+
+
+function cube (x) {
+return (x*x*x)
+}
+
+> cube(4)    // returns 64
+
+2. Absolute
+
+function absolute ()
+{
+
+
+}
+
+3. Names
+
+var names = ['Bill', 'Carl', 'Kat', 'Emil', 'Cath']
+> function printNames (names) {
+... console.log(names + 'y')
+... }
+
+> names.forEach(printNames)   // returns Billy Carly Cathy Emily Katy
+
+4. Make Kia Ora
+
+function makeKiaOra (name)
+{
+  return
+}
+
+
+5. Make Since
